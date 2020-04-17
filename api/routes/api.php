e53248553591feb08962bbf8c0a2cab804bd173c
Gabriel Passarelli <gabriel@descubraomundo.com>
2020-04-17 08:49:57 -0300

    Update API routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,4 @@
 <?php
-
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,49 +11,7 @@ use Illuminate\Support\Facades\Route;
   | is assigned the "api" middleware group. Enjoy building your API!
   |
 */
-
-Route::get('accounts/{id}', function ($id) {
-    $account = DB::table('accounts')
-             ->whereRaw("id=$id")
-             ->get();
-
-    return $account;
-});
-
-Route::get('accounts/{id}/transactions', function ($id) {
-    $account = DB::table('transactions')
-             ->whereRaw("`from`=$id OR `to`=$id")
-             ->get();
-
-    return $account;
-});
-
-Route::post('accounts/{id}/transactions', function (Request $request, $id) {
-    $to = $request->input('to');
-    $amount = $request->input('amount');
-    $details = $request->input('details');
-
-    $account = DB::table('accounts')
-             ->whereRaw("id=$id")
-             ->update(['balance' => DB::raw('balance-' . $amount)]);
-
-    $account = DB::table('accounts')
-             ->whereRaw("id=$to")
-             ->update(['balance' => DB::raw('balance+' . $amount)]);
-
-    DB::table('transactions')->insert(
-        [
-            'from' => $id,
-            'to' => $to,
-            'amount' => $amount,
-            'details' => $details
-        ]
-    );
-});
-
-Route::get('currencies', function () {
-    $account = DB::table('currencies')
-              ->get();
-
-    return $account;
-});
+Route::apiResource('currencies', 'Api\CurrencyController')->only(['index']);
+Route::apiResource('accounts', 'Api\AccountController')->only(['show']);
+Route::get('accounts/{id}/token', 'Api\AccountController@token')->name('accounts.auth');
+Route::apiResource('accounts/{id}/transactions', 'Api\TransactionController')->only(['index','store']);
