f7e039ec301d8f2311482eca805451e77e213516
Gabriel Passarelli <gabriel@descubraomundo.com>
2020-04-17 08:47:19 -0300

    Update database seeder

--- api/database/seeds/DatabaseSeeder.php
+++ api/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AccountsTableSeeder::class);
-        $this->call(TransactionsTableSeeder::class);
+        $this->call(YbankSeeder::class);
     }
 }
